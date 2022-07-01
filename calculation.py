564716580c1bdd4b766e62edb63e3966c1055412
Josephine Swann <jr21swan@siena.edu>
2022-07-01 14:46:11 -0400

    Updating to include more plots

--- calculation.py
+++ calculation.py
@@ -11,8 +11,8 @@ ehi = 500
 
 energy = np.linspace(elo,ehi,50)
 
-density = 3000 * 6e26 # Number of nucleons per m3?
-#density_m3 = density_kg * ( 6e26 # Number of nucleons per kg
+density = 1.6e30 # Number of nucleons per m3?
+#density_m3 = density_kg * ( 6e26) # Number of nucleons per kg
 
 length = 1 # length of "target" in meters?
 
@@ -41,7 +41,7 @@ N_at_CMS = np.zeros(len(energy))
 # These will be at different distances from CMS
 
 plt.figure()
-for distance in range(1,200,1):
+for distance in range(1,10,1):
 
     # Calculate the number of neutrinos coming out for 1 m^2 of rock of length=length
     # per steradian, at some given energy
@@ -61,7 +61,7 @@ for distance in range(1,200,1):
     # Attenuation -- energy loss!
     N_at_CMS_from_this_distance = np.zeros(len(energy))
     for i,e in enumerate(energy):
-        ke_final= etools.energy_loss_per_distance_traveled(e*1e9, distance, step_size=0.1)
+        ke_final= etools.energy_loss_per_distance_traveled(e*1e9, distance, step_size=.1)
         ke_final /= 1e9
         idx, central = etools.find_the_number(ke_final, energy)
         #print(e, ke_final, idx, central)
@@ -77,15 +77,18 @@ for distance in range(1,200,1):
 
     plt.plot(energy,N_at_CMS_from_this_distance,label=f'distance={distance}')
     plt.xscale('log')
+    plt.xlabel("energy")
+    plt.ylabel("number")
+    plt.title("energy vs number at CMS (per second)")
+    plt.savefig("energy_vs_number_per_second")
     ##plt.yscale('log')
-    plt.legend()
+    #plt.legend()
 
 
 
 # Now do for CMS 
 #N_CMS = N*area_of_CMS
 N_CMS_month = N_at_CMS * 3e7 /  12
-
 plt.figure()
 #plt.plot(energy, N)
 plt.plot(energy, N_at_CMS,label='# muons at CMS/s')
@@ -96,5 +99,102 @@ plt.yscale('log')
 plt.legend()
 integrated = integrate.trapz(N_CMS_month, energy)
 print(f"The integrated number is {integrated} per month for an energy range between {elo} and {ehi} GeV")
+plt.title("Number at CMS per month")
+plt.savefig("integrated_number per month.png")
+
+################################################################################
+
 
 #plt.show()
+en=np.arange(1,201)
+print(en)
+enlist=en.tolist()
+'''
+#print(enlist)
+xsec_for_mult_plot=yfunction_xsec[enlist]
+flux_for_mult_plot=yfunction_flux[enlist]
+mult_plot= xsec_for_mult_plot*flux_for_mult_plot
+plt.plot(mult_plot,enlist)
+plt.show()
+'''
+
+flux_new=etools.neutrino_flux(enlist)
+#fluxy=ypts
+xsec_new=etools.neutrino_cross_section(enlist)
+plt.figure()
+#print(ypts)
+#print(flux_new[0])
+tot= flux_new[0]*xsec_new[0]*density*en[-1]
+#print(tot)
+plt.plot(en, tot)
+#plt.xscale("log")
+plt.yscale("log")
+plt.xlabel("energy")
+plt.ylabel("flux*cross section")
+plt.show()
+
+#print(en[-1])
+
+
+################################################################################
+
+distance1= 0 
+
+N= density*length*y_flux_new*y_xsec_new
+Omega=etools.solid_angle(CMS_length, CMS_width/2, distance)
+N*=Omega
+hemisphere_area=2*np.pi*(distance**2)
+N*=hemisphere_area
+print(f"distance: {distance1}   Omega: {Omega}.   A_hemi: {hemisphere_area}")
+N_at_CMS_from_this_distance1=np.zeros(len(energy))
+for i,e in enumerate(energy):
+  ke_final=etools.energy_loss_per_distance_traveled(e*1e9, distance1, step_size=.1)
+  ke_final/=1e9
+  idx, central= etools.find_the_number(ke_final, energy)
+  if idx is not None:
+    N_at_CMS_from_this_distance1[idx]+= N[i]
+    #x1.append(N_at_CMS_from_this_distance1[idx])
+N_at_CMS+= N_at_CMS_from_this_distance1
+#plt.plot(energy, N_at_CMS_from_this_distance1, label=f"distance={distance1}")
+plt.xscale("log")
+plt.legend()
+N_CMS_month= N_at_CMS*3e7/12
+
+distance2= 20
+N= density*length*y_flux_new*y_xsec_new
+Omega=etools.solid_angle(CMS_length, CMS_width/2, distance)
+N*=Omega
+hemisphere_area=2*np.pi*(distance**2)
+N*=hemisphere_area
+print(f"distance: {distance1}   Omega: {Omega}.   A_hemi: {hemisphere_area}")
+N_at_CMS_from_this_distance2=np.zeros(len(energy))
+for i,e in enumerate(energy): 
+  ke_final=etools.energy_loss_per_distance_traveled(e*1e9, distance2, step_size=.1)
+  ke_final/=1e9
+  idx, central= etools.find_the_number(ke_final, energy)
+  if idx is not None:
+    N_at_CMS_from_this_distance2[idx]+= N[i]
+    #x1.append(N_at_CMS_from_this_distance1[idx])
+N_at_CMS+= N_at_CMS_from_this_distance1
+#plt.plot(energy, N_at_CMS_from_this_distance1, label=f"distance={distance1}")
+plt.xscale("log")
+plt.legend()
+N_CMS_month= N_at_CMS*3e7/12
+
+
+N_at_CMS_from_this_distance_tot= N_at_CMS_from_this_distance1+N_at_CMS_from_this_distance2
+
+plt.plot(energy, N_at_CMS_from_this_distance1, label=f"Distance= {distance1}")
+
+plt.plot(energy, N_at_CMS_from_this_distance2, label=f"Distance= {distance2}")
+plt.plot(energy, N_at_CMS_from_this_distance_tot, label="sum") 
+plt.xscale("log")
+plt.legend()
+plt.xlabel("energy (eV)")
+plt.ylabel("Number at CMS")
+plt.show()
+
+##########################################################################################
+
+
+
